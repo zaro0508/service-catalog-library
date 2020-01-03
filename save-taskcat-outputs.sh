89c3372df4ea0c88fb6ed76559da650a1a5806bf
Khai Do <zaro0508@gmail.com>
2020-01-03 13:52:11 -0800

    fix shell script

--- save-taskcat-outputs.sh
+++ save-taskcat-outputs.sh
@@ -6,5 +6,5 @@ REPO_NAME="${PWD##*/}"
 S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-taskcat-TastcatSynapseBucket'].Value" --output text)
 S3_BUCKET_PATH="$REPO_NAME/$TRAVIS_BUILD_NUMBER"
 S3_BUCKET_URL="s3://$S3_BUCKET/$S3_BUCKET_PATH"
-SOURCE_DIR=taskcat_outputs
+SOURCE_DIR="taskcat_outputs"
 aws s3 cp --recursive ${SOURCE_DIR} $S3_BUCKET_URL/

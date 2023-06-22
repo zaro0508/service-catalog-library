84bae935d7ce17928f13bfb8468bad72ab3ede2f
Khai Do <zaro0508@gmail.com>
2023-06-22 09:35:31 -0700

    fix GH action env vars

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -2,10 +2,6 @@
 set -e
 
 # Deploy templates to AWS Admincentral account to share with other projects.
-REPO_NAME="${PWD##*/}"
-S3_BUCKET="bootstrap-awss3cloudformationbucket-19qromfd235z9"    # bucket in org-sagebase-admincentral
-S3_BUCKET_PATH="$REPO_NAME/${{ github.ref_name }}"
-S3_BUCKET_URL="s3://$S3_BUCKET/$S3_BUCKET_PATH"
 
 # Clean existing files on S3 bucket
 aws s3 rm --recursive $S3_BUCKET_URL/
